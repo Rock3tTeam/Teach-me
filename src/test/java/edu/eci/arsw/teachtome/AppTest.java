3c9a9166721379303ae82ce4fea6912b43e5b259
Silenrate <daniwalteros596@gmail.com>
2020-09-12 19:26:04 -0500

    Codacy Fix

--- src/test/java/edu/eci/arsw/teachtome/AppTest.java
+++ src/test/java/edu/eci/arsw/teachtome/AppTest.java
@@ -17,8 +17,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.junit.Assert.assertEquals;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @RunWith(SpringRunner.class)
