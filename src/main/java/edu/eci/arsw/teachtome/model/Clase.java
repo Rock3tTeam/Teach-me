f34240810fec124660def29bc58721ad0bfbfd9b
Silenrate <daniwalteros596@gmail.com>
2020-09-14 20:11:05 -0500

    Codacy Fix

--- src/main/java/edu/eci/arsw/teachtome/model/Clase.java
+++ src/main/java/edu/eci/arsw/teachtome/model/Clase.java
@@ -1,6 +1,14 @@
 package edu.eci.arsw.teachtome.model;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import java.sql.Timestamp;
 
 /**
@@ -27,10 +35,10 @@ public class Clase {
     private int amountOfStudents;
 
     @Column(name = "date_of_init")
-    private java.sql.Timestamp dateOfInit;
+    private Timestamp dateOfInit;
 
     @Column(name = "date_of_end")
-    private java.sql.Timestamp dateOfEnd;
+    private Timestamp dateOfEnd;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "professor")
