688cb6b5ba8e3e06020cac6b08be4b20c2c15571
juancamilo399 <juan.angel-h@mail.escuelaing.edu.co>
2020-09-18 16:04:34 -0500

    select and profile design

---
+++ src/main/resources/static/js/accept.js
@@ -0,0 +1,14 @@
+
+
+/**
+ * $('select').selectpicker();
+function select_All(){
+    $('select').selectpicker('selectAll');
+}
+ */
+
+
+$('#btn').on('click', function() {
+    console.log($('#select_students').val());
+});
+
