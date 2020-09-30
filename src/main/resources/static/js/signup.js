8048ee705a17a8e06706a28e82cae8e952e0b35f
juancamilo399 <juan.angel-h@mail.escuelaing.edu.co>
2020-09-30 17:51:15 -0500

    validator

--- src/main/resources/static/js/signup.js
+++ src/main/resources/static/js/signup.js
@@ -1,5 +1,34 @@
 var ModuleSignup = (function () {
 
+    function validate(email,firstName,lastName,password){
+        var bool = true;
+
+        if (firstName=="") {
+            bool = false;
+            console.log("nombre")
+        }
+
+        else if (lastName==""){
+            bool = false;
+            console.log("ape")
+        }
+
+        else if (password==""){
+            bool = false;
+            console.log("pass")
+        }
+
+        else if (!(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email))){
+            bool = false;
+            console.log("email")
+        }
+
+        return bool;
+
+    }
+
+
+
     function createUser(){
 
         var email = document.getElementById("email").value;
@@ -7,18 +36,26 @@ var ModuleSignup = (function () {
         var lastName = document.getElementById("last_name").value;
         var password = document.getElementById("pass").value;
         var description = document.getElementById("description_signup").value;
+        var bool=validate(email,firstName,lastName,password);
 
-        var user = {
-            "email":email,
-            "firstName":firstName,
-            "lastName":lastName,
-            "password": password,
-            "description": description
-        };
-        console.log(user);
-        apiclient.postUser(user).then(function (){
-            console.log("post succesful");
-        });
+        if(bool){
+            var user = {
+                "email":email,
+                "firstName":firstName,
+                "lastName":lastName,
+                "password": password,
+                "description": description
+            };
+            console.log(user);
+            apiclient.postUser(user).then(function (){
+                console.log("post succesful");
+            });
+        }
+
+
+
+
+
     };
 
 
