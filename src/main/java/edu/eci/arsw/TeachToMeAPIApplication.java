eb2175be9fe0b181e0cca060b021d801be27e09d
NicolasAguilera9906 <nicolasaguileracontreras@gmail.com>
2020-10-02 17:50:43 -0500

    webmvcconfigurer

--- src/main/java/edu/eci/arsw/TeachToMeAPIApplication.java
+++ src/main/java/edu/eci/arsw/TeachToMeAPIApplication.java
@@ -2,7 +2,11 @@ package edu.eci.arsw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * Clase Main de la aplicación Teach To Me
@@ -13,6 +17,16 @@ public class TeachToMeAPIApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TeachToMeAPIApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
     }
 }
 
