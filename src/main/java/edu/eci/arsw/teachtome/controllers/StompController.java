884f15e5d2b1c318bfd29d6a26aa1173f3268771
Silenrate <daniwalteros596@gmail.com>
2020-12-05 14:13:03 -0500

    Codacy Fix

--- src/main/java/edu/eci/arsw/teachtome/controllers/StompController.java
+++ src/main/java/edu/eci/arsw/teachtome/controllers/StompController.java
@@ -1,6 +1,5 @@
 package edu.eci.arsw.teachtome.controllers;
 
-import edu.eci.arsw.teachtome.model.Draw;
 import edu.eci.arsw.teachtome.model.Message;
 import edu.eci.arsw.teachtome.model.Point;
 import edu.eci.arsw.teachtome.services.TeachToMeServices;
@@ -9,8 +8,6 @@ import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-
-import java.util.ArrayList;
 
 /**
  * Controlador de estilo publish suscribe para los eventos ocurridos en las sesiones de clase de Teach To Me
